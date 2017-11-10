7685695612c43ef60f6e6498bc1ec122e87269a5
Oo- thero -oO <kevin.chambelland@outlook.com>
2017-11-10 08:51:26 +0100

    plateau

--- Ninjago/Vue/Plateau_Jeu.xaml.cs
+++ Ninjago/Vue/Plateau_Jeu.xaml.cs
@@ -20,10 +20,15 @@ namespace Ninjago.Vue
     public partial class Plateau_Jeu 
     {
         String choixMain;
+        
         public Plateau_Jeu()
         {
             InitializeComponent();
-            
+            Main.Visibility = Visibility.Hidden;
+            C1.Visibility = Visibility.Hidden;
+            C2.Visibility = Visibility.Hidden;
+            C3.Visibility = Visibility.Hidden;
+            C4.Visibility = Visibility.Hidden;
         }
         private void btn_retour_plateau_Click(object sender, RoutedEventArgs e)
         {
@@ -36,6 +41,7 @@ namespace Ninjago.Vue
         {
             Button btn = sender as Button;
             btn.Content = choixMain;
+           
             C1.IsChecked = false;
             C2.IsChecked = false;
             C3.IsChecked = false;
@@ -44,16 +50,72 @@ namespace Ninjago.Vue
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         { 
-            
             RadioButton choix = sender as RadioButton;
             choixMain=choix.Content.ToString();
         }
-
-        private void btn_retour_plateau_Click(object sender, RoutedEventArgs e)
+        Boolean retourner = true;
+        private void btn_jouer_Click(object sender, RoutedEventArgs e)
         {
-            Launcher fenetre = new Launcher();
-            fenetre.Show();
-            this.Close();
+            Main.Visibility = Visibility.Hidden;
+            C1.Visibility = Visibility.Hidden;
+            C2.Visibility = Visibility.Hidden;
+            C3.Visibility = Visibility.Hidden;
+            C4.Visibility = Visibility.Hidden;
+            int val_ret;
+            if (btn_jouer.Content.ToString()== "Jouer") {
+                if (retourner == true)
+                {
+                    val_ret = 0;
+                    retourner = false;
+                }
+                else
+                {
+                    val_ret = 180;
+                    retourner = true;
+                }
+                RotateTransform rotateTransform = new RotateTransform(val_ret);
+                rotateTransform.CenterX = 0;
+                rotateTransform.CenterY = 0;
+                Plateau.RenderTransform = rotateTransform;
+                btn_jouer.Content = "Passer";
+                
+                Main.Visibility= Visibility.Visible;
+                C1.Visibility = Visibility.Visible;
+                C2.Visibility = Visibility.Visible;
+                C3.Visibility = Visibility.Visible;
+                C4.Visibility = Visibility.Visible;
+
+
+
+                for (int i = 1; i <= 3; i++)
+                {
+                    RotateTransform rotateTransformBtn = new RotateTransform(val_ret);
+                    rotateTransformBtn.CenterX = 40;
+                    rotateTransformBtn.CenterY = 40;
+                    
+
+                    Button btn1 = (Button)FindName("btn1_"+i);
+                    if (btn1.Content.Equals(""))
+                    {
+                        btn1.RenderTransform = rotateTransformBtn;
+                    }
+                    Button btn2 = (Button)FindName("btn2_" + i);
+                    if (btn2.Content.Equals(""))
+                    {
+                        btn2.RenderTransform = rotateTransformBtn;
+                    }
+                    Button btn3 = (Button)FindName("btn3_" + i);
+                    if (btn3.Content.Equals(""))
+                    {
+                        btn3.RenderTransform = rotateTransformBtn;
+                    }
+                }
+            }
+           else
+            {
+                btn_jouer.Content = "Jouer";
+            }
         }
+        
     }
 }
