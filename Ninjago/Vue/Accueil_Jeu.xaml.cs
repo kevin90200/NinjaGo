651f0df4aef2d320407be1707b7bd9446b96f25f
Oo- thero -oO <kevin.chambelland@outlook.com>
2017-11-14 15:02:28 +0100

    accueil_jeu 14/11/2017/16h

--- Ninjago/Vue/Accueil_Jeu.xaml.cs
+++ Ninjago/Vue/Accueil_Jeu.xaml.cs
@@ -15,6 +15,7 @@ using MahApps.Metro.Controls;
 using System.Net;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.IO;
 namespace Ninjago.Vue
 {
     /// <summary>
@@ -22,7 +23,8 @@ namespace Ninjago.Vue
     /// </summary>
     public partial class Accueil_Jeu
     { List<Joueur> lesJ = new List<Joueur>();
-        List<Carte> maCollection = new List<Carte>();            //collection du joueur (toutes les cartes qui ont un exemplaire > 0)
+        List<CartePersonnage> lesCartes = new List<CartePersonnage>();
+        List<CartePersonnage> maCollection = new List<CartePersonnage>();            //collection du joueur (toutes les cartes qui ont un exemplaire > 0)
         List<Carte> monDeck = new List<Carte>();                //deck du joueur, liste de carte utilisé pour le jeu
         Carte carte;                                            //Correspond à la carte séléctionnée dans l'interface (peut être null)
         CarteAction carteAction = new CarteAction();
@@ -36,17 +38,91 @@ namespace Ninjago.Vue
 
         private void btn_jouer_Click(object sender, RoutedEventArgs e)
         {
-            Joueur J1 = new Joueur(PseudoJ1.ToString(), "" );
+            Joueur J1 = new Joueur(PseudoJ1.ToString(), "");
             Joueur J2 = new Joueur(PseudoJ2.ToString(), "");
-            J1.DateNaissance= Convert.ToDateTime(DateJ1);
-            J2.DateNaissance = Convert.ToDateTime(DateJ2);
-            if (MonDeckJ1.IsChecked == true)
-            {foreach (Carte d in monDeck)
-                {if(d.Type == "p")
-                    J1.Deck.Add(d);
+
+
+            var json = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText("ninjago.json"));
+            //Parcours de la collection pour créer les cartes en fonction du type renvoyer par le JSON
+
+            foreach (var carte in json)
+            {
+                Carte c = new Carte(carte.Nom.ToString(), carte.Numero.ToString(), Convert.ToInt32(carte.Exemplaire), carte.Description.ToString(), carte.Type.ToString(), Convert.ToBoolean(carte.Deck));  //GetValue("xxx") permet de récupérer les données du JSON
+                if (c.Type == "P")
+                {
+                    CartePersonnage cp = new CartePersonnage(carte.Nom.ToString(), carte.Numero.ToString(), Convert.ToInt32(carte.Exemplaire), carte.Description.ToString(), carte.Type.ToString(), Convert.ToBoolean(carte.Deck), Convert.ToInt32(carte.Attaque), Convert.ToInt32(carte.Defense), Convert.ToInt32(carte.Vitesse), Convert.ToInt32(carte.Force));
+                    lesCartes.Add(cp);
+                    if (cp.Deck == true)
+                    {
+                        monDeck.Add(cp);
+                    }
+                    if (cp.Exemplaire > 0)
+                    {
+                        maCollection.Add(cp);
+                    }
+                }
+
+            }
+            if (MonDeckJ1.IsChecked == true || MonDeckJ2.IsChecked == true)
+            { 
+                foreach (CartePersonnage cp in monDeck)
+                {
+                     if (cp.Deck == true)
+                     {
+                          if (MonDeckJ1.IsChecked == true)
+                          {
+                                
+                            J1.Deck.Add(cp);
+                          }
+                    
+                          if (MonDeckJ2.IsChecked == true)
+                            {
+                            J2.Deck.Add(cp);
+                        
+                            }
+                     }
                 }
             }
-           
+            if(AleatoireColJ1.IsChecked == true || AleatoireColJ2.IsChecked ==true)
+            {
+                int nombreCarteCollection = maCollection.Count();
+                
+                for (int i = 1; i <= 20; i++)
+                {
+                    Random aleatoire = new Random();
+                    int numeroAlea = aleatoire.Next(nombreCarteCollection - 1);
+                    if(AleatoireColJ1.IsChecked == true)
+                    {
+                        J1.Deck.Add(maCollection[numeroAlea]);
+                    }
+                    if (AleatoireColJ2.IsChecked == true)
+                    {
+                        J2.Deck.Add(maCollection[numeroAlea]);
+                    }
+                }
+            }
+            if (AleatoireAllJ1.IsChecked == true || AleatoireAllJ2.IsChecked == true)
+            {
+                int nombreCarteAll = lesCartes.Count();
+
+                for (int i = 1; i <= 20; i++)
+                {
+                    Random aleatoire = new Random();
+                    int numeroAlea = aleatoire.Next(nombreCarteAll - 1);
+                    if (AleatoireColJ1.IsChecked == true)
+                    {
+                        J1.Deck.Add(lesCartes[numeroAlea]);
+                    }
+                    if (AleatoireColJ2.IsChecked == true)
+                    {
+                        J2.Deck.Add(lesCartes[numeroAlea]);
+                    }
+                }
+            }
+
+
+
+
         }
 
 
